74fed05e1078d21e9269161b316dafe0cf9cb184
Ibtissamib <ibboutayeb@gmail.com>
2023-04-27 10:16:34 -0700

    task0 & 1

--- _printf.c
+++ _printf.c
@@ -4,6 +4,91 @@
 #include <stdlib.h>
 
 /**
+ * _print_NULL- prints null
+ * @len: length of what preceds
+ *
+ * Return: new length after printing
+ */
+int _print_NULL(int len)
+{
+	char *c = "(null)";
+
+	while (*c)
+	{
+		write(1, c, 1);
+		c++;
+	}
+	len = len + 6;
+	return (len);
+}
+
+/**
+ * _print_int - prints integer
+ * @len: length of what preceds
+ * @n: integer to print
+ * Return: new length after printing string
+ */
+
+int _print_int(int len, int n)
+{
+	unsigned int n1 = n;
+	int nbr, i = 1;
+	char c;
+
+	if (n < 0)
+	{
+		c = '-';
+		write(1, &c, 1);
+		len++;
+		n1 = -n1;
+	}
+	nbr = n1;
+	while (nbr >= 10)
+	{
+		while (nbr >= 10)
+		{
+			nbr = nbr / 10;
+			i = i * 10;
+		}
+		c = nbr + '0';
+		write(1, &c, 1);
+		len++;
+		nbr = n1 % i;
+		while (nbr < (i / 10))
+		{
+			i = i / 10;
+			c = '0';
+			write(1, &c, 1);
+			len++;
+			nbr = nbr % 10;
+		}
+		i = 1;
+	}
+	c = (nbr % 10) + '0';
+	write(1, &c, 1);
+	len++;
+	return (len);
+}
+
+/**
+ * _print_string - prints string
+ * @len: length of what preceds
+ * @p: string to print
+ * Return: new length after printing string
+ */
+
+int _print_string(int len, char *p)
+{
+	while (*p)
+	{
+		write(1, p, 1);
+		len++;
+		p++;
+	}
+	return (len);
+}
+
+/**
  * _printf-prints
  * @format: const pointer to char
  * ...: arguments
