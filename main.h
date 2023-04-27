a562148a1275388dc9b979f55620c47ea97b9428
Ot-Sara <saraotmani95@gmail.com>
2023-04-26 18:31:04 -0700

    print int

--- main.h
+++ main.h
@@ -1,15 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
-<<<<<<< HEAD
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
+int _printf_string(int len, char *p);
+int _print_int(int len, int n);
 int _printf(const char *format, ...);
 
-#endif
-=======
-int _printf(const char *format, ...);
 #endif /* MAIN_H */
->>>>>>> 9c18341207528cb56ac8c434c68c9339ee2b88e0
+
