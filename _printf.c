0fde8d42f25f50017d8db94da6b43cfbcd7408d5
Ot-Sara <saraotmani95@gmail.com>
2023-04-26 18:09:24 -0700

    print int

--- _printf.c
+++ _printf.c
@@ -1,86 +1,114 @@
 #include "main.h"
-<<<<<<< HEAD
-
-/**
- * _printf - prints anything
- * @format: the format string
- * Return: number of bytes printed
- */
-int _printf(const char *format, ...)
-{
-	int len = 0, nbr;
-=======
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
+
 /**
- * _printf-print characters
+ * _print_string - prints string
+ * @len: length of what preceds
+ * @p: string to print
+ * Return: new length after printing string
+ */
+int _print_string(int len, char *p)
+{
+	while (*p)
+	{
+		write(1, p, 1);
+		len++;
+		p++; }
+	return (len);
+}
+/**
+ * _print_int - prints integer
+ * @len: length of what preceds
+ * @n: integer to print
+ * Return: new lentgh after printing integer
+ */
+int _print_int(int len, int n)
+{
+	unsigned int n1 = n;
+	int nbr, i = 1;
+	char c;
+
+	if (n < 0)
+	{
+		c = '-';
+		write(1, &c, 1);
+		len++;
+		n1 = -n1;
+	}
+	nbr = n1;
+	while (nbr >= 10)
+	{
+		while (nbr >= 10)
+		{
+			nbr = nbr / 10;
+			i = i * 10;
+		}
+		c = nbr + '0';
+		write(1, &c, 1);
+		len++;
+		nbr = n1 % i;
+		while (nbr < (i / 10))
+		{
+			i = i / 10;
+			c = '0';
+			write(1, &c, 1);
+			len++;
+			nbr = nbr % i;
+		}
+		i = 1;
+	}
+	c = (nbr % 10) = '0';
+	write(1, &c, 1);
+	len++;
+	return (len);
+}
+/**
+ * _printf - prints characters
  * @format: const pointer to char
  * ...: arguments
- *
- * Return: Integer
+ * Return: integer
  */
 int _printf(const char *format, ...)
 {
-	int len = 0;
->>>>>>> 9c18341207528cb56ac8c434c68c9339ee2b88e0
+	int n, len = 0;
 	char c, *p;
 	va_list ap;
 
-	if (format == NULL || *format == '\0')
+	if (format == NULL || *format == '/0')
 		return (-1);
 	va_start(ap, format);
 	while (*format)
 	{
-		if (*format == '%' && *(format - 1) != '%' && *(format + 1) == '\0')
+		if (*format == '%' && (*(format + 1) == 'd' || *(format + 1) == 'i'))
+		{	n = va_arg(ap, int);
+			len = _print_int(len, n);
+			format++; }
+		else if (*format == '%' && *(format - 1) != '%' && *(format + 1) == '\0')
 			return (-1);
-<<<<<<< HEAD
-		else if ((*format != 'c' && *format != 's' && *format != '%' &&
-				*format != 'd' && *format != 'i') || ((*format == 'c'
-				 || *format == 's' || *format == 'd' || *format == 'i') &&
-				*(format - 1) != '%') || (*format == '%' && (*(format + 1) == '%'
-				|| (*(format - 1) != '%' && *(format + 1) != 'c' && *(format + 1)
-				!= 's' && *(format + 1) != 'd' && *(format + 1) != 'i'))))
+		else if (*format == '%' && *(format + 1) == '%' && (*(format + 2) != 's'
+					&& *(format + 2) != 'c' && *(format + 2) != 'd' && *(format + 2) != 'i'))
 		{	write(1, format, 1);
-			len++; }
-		else if ((*format == 'c') && *(format - 1) == '%')
-		{
-			c = va_arg(ap, int);
-=======
-		else if ((*format != 'c' && *format != 's' && *format != '%')
-				|| ((*format == 'c' || *format == 's') && *(format - 1) != '%')
-				|| (*format == '%' && (*(format + 1) == '%' ||
-						(*(format - 1) != '%' && *(format + 1)
-						 != 'c' && *(format + 1) != 's'))))
-		{	write(1, format, 1);
-			len++; }
-		else if ((*format == 'c') && *(format - 1) == '%')
+			len++;
+			format++; }
+		else if (*format == '%' && *(format + 1) == 'c')
 		{	c = va_arg(ap, int);
->>>>>>> 9c18341207528cb56ac8c434c68c9339ee2b88e0
 			write(1, &c, 1);
-			len++; }
-		else if (*format == 's' && *(format - 1) == '%')
-		{
-			p = va_arg(ap, char *);
+			len++;
+			format++; }
+		else if (*format == '%' && *(format + 1) == 's')
+		{	p = va_arg(ap, char *);
 			if (p == NULL)
 			{	exit(2);
 				return (0); }
-			while (*p)
-			{	write(1, p, 1);
-				len++;
-<<<<<<< HEAD
-				p++; } }
-		else if ((*format == 'd' || *format == 'i') && *(format - 1) == '%')
-		{	nbr = va_arg(ap, int);
-			len = _printf(len, nbr); }
-		format++; }
-	va_end(ap);
-=======
-				p++; }
-		}
-		format++;
-	}
-	va_end(ap);
-	return (len);
->>>>>>> 9c18341207528cb56ac8c434c68c9339ee2b88e0
-}
+			len = _print_string(len, p);
+			format++; }
+		else if ((*format == 'c' || *format == 's' || *format == '%' || *format ==
+					'd' || *format == 'i') && *(format - 1) == '%')
+			format++;
+		else
+		{	write(1, format, 1);
+			len++;
+			format++; } }
+	return (len); }
