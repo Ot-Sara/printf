78ceb6145806ba6df7849f0914df9ed5cbe013ad
Ibtissamib <ibboutayeb@gmail.com>
2023-04-27 10:33:48 -0700

    task0 & 1

--- _printf.c
+++ _printf.c
@@ -43,6 +43,13 @@ int _print_int(int len, int n)
 		n1 = -n1;
 	}
 	nbr = n1;
+	if (nbr < 10)
+	{
+		c = nbr + '0';
+		write(1, &c, 1);
+		len++;
+		return (len);
+	}
 	while (nbr >= 10)
 	{
 		while (nbr >= 10)
