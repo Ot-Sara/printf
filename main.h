3607f25b807bc16061784bf069eb65656d55a8db
Ibtissamib <ibboutayeb@gmail.com>
2023-04-27 07:32:34 -0700

    main.h

--- main.h
+++ main.h
@@ -1,4 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _printf(const char *format, ...);
+int _print_string(int len, char *p);
+int _print_int(int len, int n);
+int _print_NULL(int len);
 #endif /* MAIN_H */
