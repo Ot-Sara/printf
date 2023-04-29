016f3ec680e0a430328d6e2556599695e36fb10a
Ibtissamib <ibboutayeb@gmail.com>
2023-04-28 18:03:29 -0700

    5. My weakness is wearing too much leopard print

--- _printf.c
+++ _printf.c
@@ -3,43 +3,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-/**_print_s_specifier- prints string and handle Non printable characters
- * @len: length of what preceds
- * @p: pointer to char
- *
- * Return: new length after printing
- */
-int _print_s_specifier(int len, char *p)
-{
-	char c;
-
-	while (*p)
-	{
-		if (*p >= 32 && *p < 127)
-		{	write(1, p, 1);
-			len++;
-		}
-		else
-		{
-			c = '\\';
-			write (1, &c, 1);
-			len++;
-			c = 'x';
-			write (1, &c, 1);
-			len++;
-			if (*p <= 15)
-			{
-				c = '0';
-				write(1, &c, 1);
-				len++;
-			}
-			len = _convert_HEX(len, *p);
-		}	
-		p++;
-	}
-	return (len);
-
-}
 /**
  * _convert_oct - converts in octal and prints
  * @n: integer to convert
